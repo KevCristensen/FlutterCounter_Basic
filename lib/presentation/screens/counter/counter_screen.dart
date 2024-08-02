0f9a10b3d70cda125f6cdcce1600bda9b4f74bb4
Kevin Cristensen <ke.cristensen@duocuc.cl>
2024-08-02 13:48:33 -0400

    Base para empezar app contador

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -6,7 +6,7 @@ class CounterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-        body: Center(child: Text('Hola LMundo')),
+        body: Center(child: Text('Hola Mundo')),
       );
   }
 }
